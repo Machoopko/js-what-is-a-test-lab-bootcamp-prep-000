c3730c6a130c11048156253d23f37c5e186076dc
Marina Kosovic <marinakosovic15@gmail.com>
2017-09-16 12:37:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // write your code below!
 
-
+var name = "Susan"
+var height = "35"
 var message = ` is ${height} inches tall`
 
 module.exports = {name, height, message}
 
-var name = "Susan"
-var height = "35"
+
